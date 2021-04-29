b45df1b12ce8118738faed894a3a20c9dd14ea25
l0l67 <jonathann34555@gmail.com>
2021-04-29 05:26:09 +0200

    v3

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import random
 
 
 
-circle1_pos_x = 50
-circle1_pos_y = 50
+circle1_pos_x = 250
+circle1_pos_y = 250
+mouseX = 250
+mouseY = 250
 delay = 0
 x_drag = 0
 y_drag = 0
@@ -13,40 +15,34 @@ override = False
 key = cv.waitKey(1)
 
 def get_pointer_coords(event,x,y,flags,param):
-    global mouseX,mouseY
+    global mouseX,mouseY,delay,circle1_pos_x,circle1_pos_y,x_drag,y_drag
+
     if event == cv.EVENT_LBUTTONDOWN:
         mouseX,mouseY = x,y
-        print(mouseX, mouseY)
-        global delay
-        global circle1_pos_x
-        global circle1_pos_y
-        global x_drag
-        global y_drag
-
-        delay = random.randint(50, 100)
-        circle1_pos_x = mouseX
-        circle1_pos_y = mouseY
-
         x_drag = random.randint(1, 10)
         y_drag = random.randint(1, 10)
+        circle1_pos_x = 250
+        circle1_pos_y = 250
+        delay = 10
+
 
 
 while(True):
     img = np.zeros((500, 500, 3), np.uint8)
 
-    
-
     cv.setMouseCallback('RGB', get_pointer_coords)
 
     cv.circle(img, (circle1_pos_x, circle1_pos_y), 10, (0, 150, 255), -1)
 
-    
-
     if (circle1_pos_x < 500 and circle1_pos_x > 0 and circle1_pos_y < 500 and circle1_pos_y > 0) or override == True: 
-        if delay > 0:
-            circle1_pos_x += int(delay / x_drag)
-            circle1_pos_y += int(delay / y_drag)
-            delay -= 1
+        if circle1_pos_x != mouseX and circle1_pos_y != mouseY:
+            if delay < 10000:
+                goto_x = int((mouseX -250))
+                goto_y = int((mouseY -250))
+                circle1_pos_x += int(goto_x / delay) 
+                circle1_pos_y += int(goto_y / delay)
+            delay += 1
+            print(delay, circle1_pos_x ,circle1_pos_y)
             override = False
     else:
         circle1_pos_y = 500 - circle1_pos_y
